9ad1fe9dbee84b04846eb07b7ba20010f4991f04
Blagovest-Todorov <blagovest.stod@abv.bg>
2020-11-20 16:42:37 +0200

    init

---
+++ Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Linq;
+using System.Text;
+
+namespace _04.substrings
+{
+    class Program
+    {
+        static void Main()
+        {
+            //string result = str1 +str2;
+            //IS  equivalent as :
+            string str1 = string.Empty; //string is immutable Type
+
+            StringBuilder sb = new StringBuilder(); //mutable Type 
+            sb.Append("Hello ");
+            sb.Append("World!");
+            sb.Append(" and Hello C#");
+            string result = sb.ToString();
+            Console.WriteLine(result);
+            
+        }
+    }
+}
